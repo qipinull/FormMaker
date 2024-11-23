7cfdd653dd8f6ff6495d44406dc8f7133a40d45e
qipinull <ebroberts9@icloud.com>
2024-11-23 21:01:39 +0000

    big upgrade
    wowie

--- scripts.js
+++ scripts.js
@@ -1,28 +1,25 @@
 console.log("script is running")
 var i = 0
 function submitGame(i) {
-    var game = document.getElementById("game").value;
-    var gameType = document.getElementById("gameType").value;
+    var game = document.getElementById("gameId").value;
+    var gameType = document.getElementById("gameTypeId").value;
     
-    if (game.length === 0) {
-        var game = ("undefined")
-    }
+        if (game === "") {
+            return
+        }
 
-    if (gameType.length === 0) {
-        var gameType = ("undefined")
-    }
+        if (gameType.length === "") {
+            return
+        }
     
-    var numGame = String(i)
     console.log(i)
-    console.log(numGame)
 
     const gameTable = document.getElementById('gameTable')
     const newGame = document.createElement(`tr`)
     newGame.innerHTML = `<td>${game}</td>
-                         <td id="${numGame}">${gameType}</td>`
+                         <td>${gameType}</td>`
 
     gameTable.appendChild(newGame)
     var i = (i++)
     return(i)
 }
-
